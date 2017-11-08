0d71d60ebd2bd72deff2e882df43190fcb950633
Unknown <ekaitznavarro@gmail.com>
2017-11-08 19:55:13 +0100

    Ejercicio 2

---
+++ 02_cambio_moneda/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void calcularCambio(double moneda, String a) {
+        double resultado;
+        resultado = 0;
+
+        switch (a) {
+            case "usd":
+                resultado = moneda * 1.15945;
+                System.out.print("El resultado es: ");
+                System.out.println(resultado);
+                break;
+
+            case "gbp":
+                resultado = moneda * 0.88435;
+                System.out.print("El resultado es: ");
+                System.out.println(resultado);
+                break;
+            case "cny":
+                resultado = moneda * 7.6841;
+                System.out.print("El resultado es: ");
+                System.out.println(resultado);
+                break;
+            case "jpy":
+                resultado = moneda * 132;
+                System.out.print("El resultado es: ");
+                System.out.println(resultado);
+                break;
+            default:
+                System.out.println("Error");
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a;
+        double moneda;
+        System.out.print("Introduzca una cantidad en euros: ");
+        moneda = Double.parseDouble(br.readLine());
+
+        System.out.print("Moneda a la que convertir (USD, GBP, CNY, JPY): ");
+        a = br.readLine();
+        calcularCambio(moneda, a);
+    }
+}
